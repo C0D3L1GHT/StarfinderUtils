1e91eb9dfa8638b40f4e52622f5a8463a9f5ff24
Tanner Reed <septem216@gmail.com>
2023-02-21 16:49:51 -0500

    finished (for the most part) the loot generator

--- RandomGenerators/EquipmentScraper.js
+++ RandomGenerators/EquipmentScraper.js
@@ -4,24 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const { JSDOM } = jsdom;
 
-const LIGHT_ARMOR_DATA_START   	= 10;
-const HEAVY_ARMOR_DATA_START   	= 10;
-const POWERED_ARMOR_DATA_START 	= 3;
-const ARMOR_UPGRADE_DATA_START 	= 8;
-const SHIELD_DATA_START        	= 8;
-const ADV_MELEE_DATA_START 	   	= 7;
-const AMMO_DATA_START		   	= 6;
-const BASIC_MELEE_DATA_START	= 8;
-const GRENADE_DATA_START		= 7;
-const HEAVY_ARMS_DATA_START		= 11;
-const LONG_ARMS_DATA_START		= 11;
-const SMALL_ARMS_DATA_START		= 11;
-const SNIPER_DATA_START			= 11;
-const SOLARIAN_DATA_START		= 7;
-const SPECIAL_DATA_START		= 11;
-const ACCESSORIES_DATA_START	= 7;
-const FUSIONS_DATA_START		= 3;
-
 async function newsFeedDateHasChanged(fileName){
   try{
     const response = await fetch('https://www.aonsrd.com/');
@@ -55,7 +37,18 @@ function getListFileDate(fileName){
   }
 }
 
-async function scrapeInfoAndAddToFile(textFile, pageLink, tableName, dataStart){
+function getListFile(fileName){
+  try {
+    var data = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' });
+    var ret = data.split('\n');
+    return ret.slice(1);
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
+
+async function scrapeInfoAndAddToFile(textFile, pageLink, tableName, levelIndex){
 	var listChanged = await newsFeedDateHasChanged(textFile);
 	if(!listChanged){
 		//console.log("no new content!");
@@ -65,14 +58,16 @@ async function scrapeInfoAndAddToFile(textFile, pageLink, tableName, dataStart){
 			const response = await fetch(pageLink);
 			const text = await response.text();
 			const dom = await new JSDOM(text);
-			const list = dom.window.document.getElementById(tableName).getElementsByTagName("a");
+			//const list = dom.window.document.getElementById(tableName).getElementsByTagName("a");
+			const list = [];
 			const table = dom.window.document.getElementsByTagName("table")[0].children[0];
 			for (var i = 1; i < table.rows.length; i++) {
-				var level = table.children[i].children[1].textContent;
-				list[i+dataStart-1].text = list[i+dataStart-1].text + " (" + level + ")"; 
-				//console.log(list[i+dataStart-1].text);
+				var name  = table.children[i].children[0].textContent;//i+dataStart-1
+				var level = table.children[i].children[levelIndex].textContent;
+				//console.log(name + " (" + level + ")");
+				list.push(name + " (" + level + ")");
 			}
-			addListToFile(list, textFile, dataStart);
+			addListToFile(list, textFile);
 		}catch(err){
 			console.log(err);
 		}
@@ -80,142 +75,112 @@ async function scrapeInfoAndAddToFile(textFile, pageLink, tableName, dataStart){
 }
 
 async function scrapeArmors(){
-	const lightArmor   = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/Armor.aspx?Category=Light', 'ctl00_MainContent_GridViewArmor', LIGHT_ARMOR_DATA_START);
-	const heavyArmor   = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/Armor.aspx?Category=Heavy', 'ctl00_MainContent_GridViewArmor', HEAVY_ARMOR_DATA_START);
-	const poweredArmor = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/PoweredArmor.aspx?ItemName=All', 'ctl00_MainContent_GridViewPoweredArmor', POWERED_ARMOR_DATA_START);
-	const armorUpgrade = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/ArmorUpgrades.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewArmorUpgrades', ARMOR_UPGRADE_DATA_START);
+	const lightArmor   = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/Armor.aspx?Category=Light', 'ctl00_MainContent_GridViewArmor', 1);
+	const heavyArmor   = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/Armor.aspx?Category=Heavy', 'ctl00_MainContent_GridViewArmor', 1);
+	const poweredArmor = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/PoweredArmor.aspx?ItemName=All', 'ctl00_MainContent_GridViewPoweredArmor', 1);
+	const armorUpgrade = await scrapeInfoAndAddToFile("ArmorList.txt", 'https://aonsrd.com/ArmorUpgrades.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewArmorUpgrades', 1);
 	console.log("\n\nupdated armor page!");
 }
 
-function getArmorListFile(){
-  try {
-    var data = fs.readFileSync("./ArmorList.txt", { encoding: 'utf8', flag: 'r' });
-    var ret = data.split('\n');
-    return ret.slice(1);
-  } catch (e) {
-    console.log(e);
-    return [];
-  }
-}
-
 async function scrapeShields(){
-    const shield = await scrapeInfoAndAddToFile("ShieldList.txt", 'https://aonsrd.com/Shields.aspx', 'ctl00_MainContent_DataElement', SHIELD_DATA_START)
+    const shield = await scrapeInfoAndAddToFile("ShieldList.txt", 'https://aonsrd.com/Shields.aspx', 'ctl00_MainContent_DataElement', 1);
 	console.log("\n\nupdated shield page!");
 }
 
-function getShieldListFile(){
-  try {
-    var data = fs.readFileSync("./ShieldList.txt", { encoding: 'utf8', flag: 'r' });
-    var ret = data.split('\n');
-    return ret.slice(1);
-  } catch (e) {
-    console.log(e);
-    return [];
-  }
-}
-
 async function scrapeWeapons(){
-	// TODO: some weapon tables have level after "category". Update the scrapeInfo to take this into account
-	const advMelee1h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=AdvMelee', 'ctl00_MainContent_GridViewWeapons1Hand', ADV_MELEE_START_DATA_START);
-	const advMelee2h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=AdvMelee', 'ctl00_MainContent_GridViewWeapons2Hands', ADV_MELEE_START_DATA_START);
-	const ammo         = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Ammo', 'ctl00_MainContent_GridViewWeapons1Hand', AMMO_DATA_START);
-	const basMelee1h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons1Hand', BASIC_MELEE_DATA_START);
-	const basMelee2h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons2Hands', BASIC_MELEE_DATA_START);
-	const grenade      = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Grenade', 'ctl00_MainContent_GridViewWeapons1Hand', GRENADE_DATA_START);
-	const heavyArms    = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons2Hands', BASIC_MELEE_DATA_START);
-	const longArms     = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Longarms', 'ctl00_MainContent_GridViewWeapons2Hands', LONG_ARMS_DATA_START);
-	const smallArms	   = await scrapeInfoAndAddToFile("WeaponList.txt",  'https://aonsrd.com/Weapons.aspx?Proficiency=SmallArms', 'ctl00_MainContent_GridViewWeapons1Hand', SMALL_ARMS_DATA_START);
-	const sniper       = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Sniper', 'ctl00_MainContent_GridViewWeapons2Hands', SNIPER_DATA_START);
-	const solarianCrys = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Solarian', 'ctl00_MainContent_GridViewWeapons1Hand', SOLARIAN_DATA_START);
-    const special1h    = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Special', 'ctl00_MainContent_GridViewWeapons1Hand', SPECIAL_DATA_START);
-	const special2h	   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Special', 'ctl00_MainContent_GridViewWeapons2Hands', SPECIAL_DATA_START);
-    const accessories  = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/WeaponAccessories.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewWeaponAccessories', ACCESSORIES_DATA_START);
-    const fusions      = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/WeaponFusions.aspx?ItemName=All', 'ctl00_MainContent_GridViewWeaponFusions', FUSIONS_DATA_START);
+	const advMelee1h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=AdvMelee', 'ctl00_MainContent_GridViewWeapons1Hand', 2);
+	const advMelee2h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=AdvMelee', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+	const basMelee1h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons1Hand', 2);
+	const basMelee2h   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+	const heavyArms    = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=BasicMelee', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+	const longArms     = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Longarms', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+	const smallArms	   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=SmallArms', 'ctl00_MainContent_GridViewWeapons1Hand', 2);
+	const sniper       = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Sniper', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+	const solarianCrys = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Solarian', 'ctl00_MainContent_GridViewWeapons1Hand', 1);
+    const special1h    = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Special', 'ctl00_MainContent_GridViewWeapons1Hand', 2);
+    const special2h	   = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Special', 'ctl00_MainContent_GridViewWeapons2Hands', 2);
+    const accessories  = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/WeaponAccessories.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewWeaponAccessories', 1);
+    const fusions      = await scrapeInfoAndAddToFile("WeaponList.txt", 'https://aonsrd.com/WeaponFusions.aspx?ItemName=All', 'ctl00_MainContent_GridViewWeaponFusions', 1);
 	
 	console.log("\n\nupdated weapons page!");
 }
 
-async function scrapeAmmo(){
-	
+async function scrapeConsumables(){
+	const ammo     = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Ammo', 'ctl00_MainContent_GridViewWeapons1Hand', 2);
+	const grenade  = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/Weapons.aspx?Proficiency=Grenade', 'ctl00_MainContent_GridViewWeapons1Hand', 1);
+	const drugs    = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/OtherItems.aspx?Category=Medicinals', 'ctl00_MainContent_GridViewOtherItems', 1);
+	const personal = await scrapeInfoAndAddToFile("ConsumablesList.txt", 'https://aonsrd.com/OtherItems.aspx?Category=Personal%20Items', 'ctl00_MainContent_GridViewOtherItems', 2);
 }
 
-async function scrapeWeaponAccessories(){
-	
+async function scrapeAugments(){
+	const cybernetics   = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/Cybernetics.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewCybernetics', 1);
+	const biotech       = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/Biotech.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewBiotech', 1);
+	const magitech      = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/Magitech.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewMagitech', 1);
+	//TODO: implement
+	//const necrografts   = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/PoweredArmor.aspx?ItemName=All', 'ctl00_MainContent_GridViewPoweredArmor', 1);
+	const personalUps   = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/PersonalUpgrades.aspx?ItemName=All&Family=None', 'ctl00_MainContent_DataListPersonalUpgrades', 1);
+	const speciesGrafts = await scrapeInfoAndAddToFile("AugmentsList.txt", 'https://aonsrd.com/SpeciesGrafts.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewSpeciesGrafts', 1);
+	console.log("\n\nupdated augments page!");
 }
 
-function scrapeAugments(){
-	
+async function scrapeTechItems(){
+	const tech = await scrapeInfoAndAddToFile("TechItemList.txt", 'https://aonsrd.com/TechItems.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewTechItems', 2);
+	console.log("\n\nupdated tech item page!");
 }
 
-function scrapeDrugs(){
-	
+async function scrapeMagicItems(){
+	const magic = await scrapeInfoAndAddToFile("MagicItemList.txt", 'https://aonsrd.com/MagicItems.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewMagicItems', 2);
+	console.log("\n\nupdated magic item page!");
 }
 
-function scrapeTechItems(){
-	
+async function scrapeHybridItems(){
+	const shield = await scrapeInfoAndAddToFile("HybridItemList.txt", 'https://aonsrd.com/HybridItems.aspx?ItemName=All&Family=None', 'ctl00_MainContent_GridViewHybridItems', 1);
+	console.log("\n\nupdated hybrid item page!");
 }
 
-function scrapeMagicItems(){
+async function getAllEquipmentByLevel(fileName, lvl){
+	let allEquipment = await getListFile(fileName);
+	let allEquipmentbyLevel = []
+	if(allEquipment.length == 0 && fileName.includes("Armor")) scrapeArmors();
+	if(allEquipment.length == 0 && fileName.includes("Weapon")) scrapeWeapons();
+	if(allEquipment.length == 0 && fileName.includes("Shield")) scrapeShields();
+	if(allEquipment.length == 0 && fileName.includes("Consumable")) scrapeConsumables();
+	if(allEquipment.length == 0 && fileName.includes("Augment")) scrapeAugments();
+	if(allEquipment.length == 0 && fileName.includes("Magic")) scrapeMagicItems();
+	if(allEquipment.length == 0 && fileName.includes("Tech")) scrapeTechItems();
+	if(allEquipment.length == 0 && fileName.includes("Hybrid")) scrapeHybridItems();
+		
+	allEquipment = await getListFile(fileName);
 	
-}
-
-function scrapeHybridItems(){
-	
-}
-
-async function getAllArmorByLevel(lvl){
-	let allArmors = await getArmorListFile();
-	let allArmorsbyLevel = []
-	if(allArmors.length == 0){
-		scrapeArmors();
-		allArmors = await getArmorListFile();
+	for(var i = 0; i < allEquipment.length; i++){
+		if (allEquipment[i].includes("("+lvl+")"))
+			allEquipmentbyLevel.push(allEquipment[i]);
 	}
-	
-	for(var i = 0; i < allArmors.length; i++){
-		if (allArmors[i].includes("("+lvl+")"))
-			allArmorsbyLevel.push(allArmors[i]);
-	}
-	return allArmorsbyLevel;
+	return allEquipmentbyLevel;
 }
 
-async function getRandomArmorByLevel(lvl){
+async function getRandomEquipmentByLevel(equipmentType, lvl){
 	if(lvl < 1 || lvl > 20)
 		console.log("invalid level: enter a number between 1 and 20")
-	var armorByLevel = await getAllArmorByLevel(lvl);
-  
-	var index = Math.floor(Math.random()*armorByLevel.length);
-	console.log(armorByLevel[index]);
-	return armorByLevel[index];
-}
-
-async function getAllShieldsByLevel(lvl){
-	let allShields = await getShieldListFile();
-	let allShieldsbyLevel = []
-	if(allShields.length == 0){
-		scrapeShields();
-		allShields = await getShieldListFile();
-	}
 	
-	for(var i = 0; i < allShields.length; i++){
-		if (allShields[i].includes("("+lvl+")"))
-			allShieldsbyLevel.push(allShields[i]);
-	}
-	return allShieldsbyLevel;
+	var equipmentByLevel = "?";
+	if (equipmentType == "Armor") equipmentByLevel = await getAllEquipmentByLevel("./ArmorList.txt",lvl);
+	if (equipmentType == "Weapon") equipmentByLevel = await getAllEquipmentByLevel("./WeaponList.txt",lvl);
+	if (equipmentType == "Shield") equipmentByLevel = await getAllEquipmentByLevel("./ShieldList.txt",lvl);
+	if (equipmentType == "Consumable") equipmentByLevel = await getAllEquipmentByLevel("./ConsumablesList.txt",lvl);
+	if (equipmentType == "Augment") equipmentByLevel = await getAllEquipmentByLevel("./AugmentsList.txt",lvl);
+	if (equipmentType == "MagicItem") equipmentByLevel = await getAllEquipmentByLevel("./MagicItemList.txt",lvl);
+	if (equipmentType == "TechItem") equipmentByLevel = await getAllEquipmentByLevel("./TechItemList.txt",lvl);
+	if (equipmentType == "HybridItem") equipmentByLevel = await getAllEquipmentByLevel("./HybridItemList.txt",lvl);
+	
+	var index = Math.floor(Math.random()*equipmentByLevel.length);
+	console.log(equipmentByLevel[index]);
+	return equipmentByLevel[index];
 }
 
-async function getRandomShieldByLevel(lvl){
-	if(lvl < 1 || lvl > 20)
-		console.log("invalid level: enter a number between 1 and 20")
-	var shieldByLevel = await getAllShieldsByLevel(lvl);
-  
-	var index = Math.floor(Math.random()*shieldByLevel.length);
-	console.log(shieldByLevel[index]);
-	return shieldByLevel[index];
-}
-
-function addListToFile(list, fileName, startIndex){
-	for(let i = startIndex; i < list.length; i++){
-		var str2add = list[i].text;
+function addListToFile(list, fileName){
+	for(let i = 0; i < list.length; i++){
+		var str2add = list[i];
 	  // str2add = str2add.replace('*', '');
 	   if(str2add[0] == ' ')
 		   str2add = str2add.slice(1, str2add.length);
@@ -226,15 +191,18 @@ function addListToFile(list, fileName, startIndex){
 }
 
 module.exports = {
-	getRandomArmorbyLevel: async function getRandomArmorByLevel(lvl){
-		if(lvl < 1 || lvl > 20)
-			console.log("invalid level: enter a number between 1 and 20")
-		var armorByLevel = await getAllArmorByLevel(lvl);
-  
-		var index = Math.floor(Math.random()*armorByLevel.length);
-		console.log(armorByLevel[index]);
-		return armorByLevel[index];
-	}
+	//TODO: add getRandomEquipments here
 }
-getRandomShieldByLevel(1);
-getRandomArmorByLevel(1);
+
+getRandomEquipmentByLevel("Armor", 1);
+getRandomEquipmentByLevel("Weapon", 1);
+getRandomEquipmentByLevel("Shield", 1);
+getRandomEquipmentByLevel("Consumable", 1);
+getRandomEquipmentByLevel("Augment", 1);
+getRandomEquipmentByLevel("MagicItem", 1);
+getRandomEquipmentByLevel("TechItem", 1);
+getRandomEquipmentByLevel("HybridItem", 1);
+
+//getRandomShieldByLevel(1);
+//getRandomArmorByLevel(1);
+//getRandomWeaponByLevel(1);
